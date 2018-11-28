462c55b52c2804f6a1b51e56b54182e2dccbc81f
wushufen <wushufen@sunlands.com>
2018-11-28 11:42:54 +0800

    完善 Class, constructor, super

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 function Class() {}
+Class.options = {}
 Class.extend = function (options) {
 	var Parent = this
 
@@ -6,19 +7,10 @@ Class.extend = function (options) {
 	// 用eval创建构造，可以指定名字
 	var Class = eval('(function ' + (options.name||'Class') + '(){_Class.apply(this,arguments)})')
 	function _Class() {
-		if (Class.__isNotSelfNew) return
-
-		// 【父类构造】
-		// 父类构造的this绑定到当前类的实例
-		Class.Parent.apply(this, arguments)
-
-		// 【字段】
-		// 复制options的字段到实例
-		for(var key in options){
-			var value = options[key]
+		// 【实例引用属性副本】
+		for(var key in this){
+			var value = this[key]
 			if (typeof value != 'function') {
-				var value = options[key]
-				this[key] = value
 				// 如果是原生【数组】或【对象】作为直接属性
 				// 则给每个实例复制一份，以免所有实例共用
 				if (value && value.constructor == Array) {
@@ -33,12 +25,20 @@ Class.extend = function (options) {
 			}
 		}
 
-		// 【初始化】
+		// 是否执行初始化
+		if (Class.__isNotSelfNew) return
+
+		// 【父类初始化】
+		// 父类初始化的this绑定到当前类的实例
+		this.constructor.super.apply(this, arguments)
+
+		// 【本类初始化】
 		// options.constructor作为初始化方法，不是真正构造函数
 		options.constructor.apply(this, arguments)
 	}
 
 	// 【继承父类】
+	// 【父类方法属性】
 	// prototype是父类的一个实例
 	// __isNotSelfNew: 父类的初始化方法此时暂不执行
 	// 当创建实例时，把父类的初始化方法绑定到当前实例
@@ -46,18 +46,19 @@ Class.extend = function (options) {
 	Class.prototype = new Parent
 	delete Parent.__isNotSelfNew
 
-	// 【方法】
+	// 【本类方法属性】
 	// 复制options的方法到原型
 	for(var key in options){
 		var value = options[key]
-		// if (typeof value == 'function') {
 		Class.prototype[key] = value
-		// }
 	}
 	// 修正prototype.constructor
 	Class.prototype.constructor = Class
 	Class.Parent = Parent
 	Class.options = options
+
+	// super
+	Class.prototype.constructor.super = Parent.options.constructor
 
 	// 当前类同样可以用.extend派生子类
 	Class.extend = Parent.extend
@@ -71,15 +72,7 @@ Class.extend = function (options) {
 var EventTarget = Class.extend({
 	name: 'EventTarget',
 	events: {},
-	constructor: function(options){
-		// :on[type]
-		for(var key in this){
-			var value = this[key]
-			if (key.match(/^on./) && typeof value == 'function') {
-				this.on(key.substr(2), value)
-			}
-		}
-	},
+	constructor: function(options){},
 	on: function(type, handler) {
 		this.events[type] = this.events[type] || []
 		this.events[type].push(handler)
@@ -99,14 +92,17 @@ var EventTarget = Class.extend({
 	trigger: function(type, event) {
 		event = event || {}
 
+		// .on('type')
 		var fns = this.events[type] || []
 		for (var i = 0; i < fns.length; i++) {
 			fns[i].apply(this, [event])
 		}
+		// .ontype
+		var handler = this['on' + type]
+		if (typeof handler == 'function') {
+			handler.apply(this, [event])
+		}
 	},
-	pauseListen: function (type) {
-		// body...
-	}
 })
 
 
@@ -149,6 +145,7 @@ var Sprite = Watcher.extend({
 		options = options || {}
 		var self = this
 		Object.assign(this, options)
+		console.log('Sprite optins', options.fpsMax, this.fpsMax, this)
 
 		// src
 		if (options.src) {
@@ -160,13 +157,8 @@ var Sprite = Watcher.extend({
 			}
 			img.src = options.src
 		}
-		
+
 		this.trigger('create')
-	},
-	onframe: function (e) {
-		this.each(function (child) {
-			child.trigger('frame', e)
-		})
 	},
 	draw: function(context){
 		this.context = context || this.context || this.parent.context
@@ -263,11 +255,9 @@ var Sprite = Watcher.extend({
 	},
 	// 事件捕获模型，从父到子传播
 	captureEvent: function (event) {
-		console.log(this.name, 'captureEvent')
 		this.trigger(event.type, event)
 
 		this.each(function (child) {
-			console.log(child)
 			if (/^(click|mousemove)$/.test(event.type)) {
 				if (child.isPointOn({x: event.offsetX, y: event.offsetY})) {
 					child.captureEvent(event)
@@ -292,7 +282,7 @@ var Game = Sprite.extend({
 	width: 0,
 	height: 0,
 	timer: null,
-	fpsMax: 1,
+	fpsMax: 100,
 	// sprites: [],
 	constructor: function(options){
 		options = options || {}
@@ -307,7 +297,10 @@ var Game = Sprite.extend({
 		this.resize()
 
 		this.addEventListener()
-		this.start()
+
+		if (this.isStart) {
+			this.start()
+		}
 	},
 	addEventListener: function(){
 		var self = this
@@ -338,9 +331,7 @@ var Game = Sprite.extend({
 	loop: function(){
 		console.log('loop')
 		var self = this
-
 		this.captureEvent({type: 'frame'})
-
 
 		this.update()
 		this.timer = setTimeout(function(){
@@ -348,6 +339,7 @@ var Game = Sprite.extend({
 		}, 1000/ this.fpsMax)
 	},
 	start: function(){
+		this.pause()
 		this.loop()
 	},
 	pause: function(){
@@ -364,7 +356,7 @@ var Fps = Sprite.extend({
 	lastTime: 0,
 	fs: 0,
 	onframe: function () {
-		console.log(this.name, 'Fps onframe')
+		console.log('Fps onframe')
 		var now = new Date
 		if (now - this.lastTime > 1000) {
 			this.lastTime = now
